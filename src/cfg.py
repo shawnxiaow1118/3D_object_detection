953d9bc221b22974a4698723ab3234946ef80912
xiaowang <wangxiaonku@gmail.com>
2017-08-06 21:23:33 -0400

    fix: fix training function and add config file

---
+++ src/cfg.py
@@ -0,0 +1,10 @@
+import os
+import os.path as osp
+import numpy as np
+# `pip install easydict` if you don't have it
+from easydict import EasyDict as edict
+
+__C = edict()
+# Consumers can get config by:
+#   from fast_rcnn_config import cfg
+cfg = __C
